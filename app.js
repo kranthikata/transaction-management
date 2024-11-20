bd88b62d12542509956fe31ed257bd4815138736
kranthikata <kranthikumarkata464@gmail.com>
2024-11-20 20:06:51 +0530

    Update: Updated app.js port

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ connectDB();
 
 // Start Server
 const port = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
